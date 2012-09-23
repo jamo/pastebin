4894b5bda6160b5ecb689a72b6c10425392bf1d8
Jarmo Isotalo <jamo@isotalo.fi>
2012-09-23 18:00:29 +0300

    nyt järkevämmin validoinnit

--- app/models/paste.rb
+++ app/models/paste.rb
@@ -1,7 +1,6 @@
 class Paste < ActiveRecord::Base
   attr_accessible :body, :filename, :nick, :title, :encoding
   validates :body, :presence => true
-  validates :title, :length => { :minimum => 3 }
   validates :key, :uniqueness => true
   LANG = ['java','text','java_script','ruby','html']
   
